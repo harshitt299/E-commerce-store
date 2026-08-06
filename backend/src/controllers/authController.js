10cff4c8433a3b7707f2502fde3fc18fcb846936
Harshit Kumar <harshitrajput95972@gmail.com>
2026-08-07 01:48:59 +0530

    Login and Logout Authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 import user from "../models";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { use } from "react";
 
 // helper function to generate jwt token
 const generateToken = (userId,role)=>{
@@ -20,7 +21,7 @@ exports.registerUser = async(req , res)=>{
         
     const existUser = await User.findOne({email})
     if(existUser){
-        res.status(400).json({success:false , message : "Email already exists!"})
+       return res.status(400).json({success:false , message : "Email already exists!"})
     }
 
     // create hash pass 
@@ -41,7 +42,7 @@ exports.registerUser = async(req , res)=>{
 
     res.cookie("token", token, {
         httpOnly : true,
-        secure : process.env.NODE_ENV=="production",
+        secure : process.env.NODE_ENV==="production",
         sameSite: "strict",
         maxAge : 7*24*60*60*1000
     });
@@ -64,4 +65,56 @@ exports.registerUser = async(req , res)=>{
 
 
 
-    
+    // login User
+
+    exports.loginUser = async (req,res)=>{
+        try {
+            const {email,password}= req.body;
+            const user = await User.findOne({email});
+            if (!user) {
+               return  res.status(400).json({success:false, message:"INVALID EMAIL or PASSWORD"})
+            }
+            const isPassworMatch = await bcrypt.compare(password,user.password)
+            if(!isPassworMatch){
+                 return  res.status(400).json({success:false, message:"INVALID EMAIL or PASSWORD"})
+            }
+
+//    generate jwt token
+
+            const token = generateToken(user._id,user.role);
+
+            // set token in htpp cookie
+            res.cookie("token" ,token, {
+                httpOnly :true,
+                secure : process.env.NODE_ENV==="production",
+                sameSite : "strict",
+                maxAge : 7*24*60*60*1000,
+            });
+
+            res.status(200).json({
+                success:true,
+                message : "Login successfully",
+                user : {
+                    _id : user._id,
+                    name : user.name,
+                    email : user.email,
+                    role: user.role,
+                }
+            });
+
+        } catch (error) {
+            res.status(500).json({success:false, message:"server Error" , error: error.message})
+        }
+    };
+
+
+
+    // logout user 
+    exports.logoutUser = (req,res)=>{
+    // for logout clear all cokkies immediately
+    res.cookie("token" , "",{
+        httpOnly :true,
+        expires : new Date(0)
+    })   
+    res.status(200).json({success:true, message: "Logout succcessfully!"});
+};
