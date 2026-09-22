bcb52a8e42d21f27c0423074141cc1401bfabe88
Harshit Kumar <harshitrajput95972@gmail.com>
2026-09-23 00:38:21 +0530

    setup routes for orders

---
+++ backend/src/routes/order.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { isAdmin, protect } from "../middleware/authMiddleware.js";
+import { createOrder , verifyPayment } from "../controllers/order.controller.js";
+
+
+const router  = express.Router();
+
+
+router.use(protect);
+
+router.post("/create" ,createOrder);
+router.post("/verify" ,verifyPayment);
+
+export default router;
+
