0cdf6df040c883f4d2981d5a082720c7b638a833
Harshit Kumar <harshitrajput95972@gmail.com>
2026-09-23 00:40:48 +0530

    fixes some issues in routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.routes.js";
 import productRouter from "./routes/product.route.js"
 import errorhandler from "./middleware/errorMiddleware.js";
 import cartRouter from "./routes/cart.routes.js";
+import orderRouter from "./routes/order.routes.js";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth" ,authRouter);
 app.use("/api/v1/products" , productRouter);
 app.use("/api/v1/cart" , cartRouter);
+app.use("/api/v1/orders" , orderRouter);
 
 
 app.get("/" , (req,res)=>{
